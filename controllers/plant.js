4cc92c87230db31c2886bebd01f173ba38192eaa
Sam Bowen <samhbowen@gmail.com>
2025-01-16 18:50:21 -0800

    async func index

--- controllers/plant.js
+++ controllers/plant.js
@@ -1,34 +1,40 @@
 const Plant = require('../models/plant');
 const moment = require('moment');
 
-
+async function index(req, res) {
+  try {
+    if(!req.session.user) {
+      return res.redirect('/auth/sign-in');
+    }
+  }
+}
 
 
 
 
 // Index
-router.get('/:userId', async (req, res) => {
-  try {
-    const userId = req.session.user._id
-    const user = await User.findById(userId).select('cabinet');
-    console.log(user, 'user');
-    res.render('plants/index.ejs', { cabinet: user.cabinet, id: userId });
-  } catch (error) {
-    console.error('Error loading cabinet: ', error);
-    res.status(500).send('Error loading cabinet');
-  }
+// router.get('/:userId', async (req, res) => {
+//   try {
+//     const userId = req.session.user._id
+//     const user = await User.findById(userId).select('cabinet');
+//     console.log(user, 'user');
+//     res.render('plants/index.ejs', { cabinet: user.cabinet, id: userId });
+//   } catch (error) {
+//     console.error('Error loading cabinet: ', error);
+//     res.status(500).send('Error loading cabinet');
+//   }
 
-});
+// });
 
 // New
-router.get('/new', (req, res) => {
-  try {
-    res.render('plants/new');
-  } catch (error) {
-    console.error('Error loading new plant form: ', error);
-    res.status(500).send('Error loading new plant form');
-  }
-});
+// router.get('/new', (req, res) => {
+//   try {
+//     res.render('plants/new');
+//   } catch (error) {
+//     console.error('Error loading new plant form: ', error);
+//     res.status(500).send('Error loading new plant form');
+//   }
+// });
 
 // Create
 router.post('/', async (req, res) => {
