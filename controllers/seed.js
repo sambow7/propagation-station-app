5ca62fcc903a3ba179bf97077c48b9330593c68c
Sam Bowen <samhbowen@gmail.com>
2025-01-16 18:41:56 -0800

    add try catch to function

--- controllers/seed.js
+++ controllers/seed.js
@@ -1,3 +1,13 @@
-const Plant = require('../models/book');
+const Plant = require('../models/plant');
 const plants = require('../models/plants');
 
+async function seedData(req, res) {
+  try {
+    await Plant.insertMany(plants);
+    res.status(200).send('Plant seeded successfully')
+  } catch (error) {
+    console.error(error, message);
+  }
+}
+
+module.exports = { seedData };
